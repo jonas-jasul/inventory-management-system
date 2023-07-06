29f5e1efe4497525b94a9ca4950a1d57bc52b857
jonas-jasul <jonas.jasul@gmail.com>
2023-07-06 15:21:20 +0300

    profile loading icon added

--- src/components/navigation/site-header.tsx
+++ src/components/navigation/site-header.tsx
@@ -1,41 +1,61 @@
 "use client"
 
-import React from 'react'
-
-import { NavigationMenuComponent } from '@/components/ui/custom/navigation-menu' 
-import AuthButton from '@/components/ui/custom/auth-button'
 import {
   SignedIn,
   SignedOut,
-  SignInButton,
   UserButton
 } from "@clerk/nextjs";
-  
+import { useEffect, useState } from 'react'
+
+import { NavigationMenuComponent } from '@/components/ui/custom/navigation-menu'
+import AuthButton from '@/components/ui/custom/auth-button'
+import { Button } from '@/components/ui/button'
+
+import Link from 'next/link'
+
 
 type Props = {}
 
+const ProfileIconSkeleton = () => {
+  return (
+    <div className='w-8 h-8 rounded-full bg-gray-200 animate-pulse'></div>
+  )
+};
+
 const SiteHeader = (props: Props) => {
-   
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
+
+
 
   return (
     <header className='h-16'>
-        <div className='h-full flex justify-between items-center max-w-7xl mx-auto'>
-            <div>
-                <h4>Logo</h4>
-            </div>
-
-            <nav className='flex gap-6 items-center'>
-                <NavigationMenuComponent />
-                <SignedIn>
-                  {/* Mount the UserButton component */}
-                  <UserButton afterSignOutUrl="/"/>
-                </SignedIn>
-                <SignedOut>
-                  {/* Signed out users get sign in button */}
-                  <SignInButton />
-                </SignedOut>
-            </nav>
+      <div className='h-full flex justify-between items-center max-w-7xl mx-auto'>
+        <div>
+          <h4>Logo</h4>
         </div>
+
+        <nav className='flex gap-6 items-center'>
+          <NavigationMenuComponent />
+          {isLoaded ? (
+            <>
+              <SignedIn>
+                {/* Mount the UserButton component */}
+                <UserButton afterSignOutUrl="/" />
+              </SignedIn>
+              <SignedOut>
+                {/* Signed out users get sign in button */}
+                <Link href='/sign-up'>
+                    <Button>Sign Up</Button>
+                </Link>
+              </SignedOut>
+            </>
+          ) : (<ProfileIconSkeleton />)}
+
+        </nav>
+      </div>
     </header>
   )
 }
